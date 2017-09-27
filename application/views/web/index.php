f9d068fd5631b8bfad51417180f0f331aadda5df
martinbedi <martinbdiaz@hotmail.com>
2017-09-27 18:23:01 -0500

    primera vista

---
+++ application/views/web/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bootstrap</title>
+		<meta charset="utf-8">
+		<!--<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		<!--<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		-->
+		<link rel="stylesheet" type="text/css" href="./assets/css/estilos.css">
+	</head>
+	<body>
+		<!--
+			xs: celulares
+			sm: tablets
+			md: computadores mini
+			lg: computadores pantalla grande
+		-->
+		<div class="encapsulador">
+			<div class="contenido">
+				<div class="container">
+					<div class="row neutralizador-row contenido-principal">
+						<div class="col-3">
+						</div>
+						<div class="col-6">
+							<h2>Eres Nuevo en Twitter</h2>
+							<h4>Registrate</h4>
+
+							<hr>
+							<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre Completo">
+							<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Correo Electronico">
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+
+							<a href="#" class="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">Registrate en Twitter</a>
+
+
+
+
+
+						</div>	
+
+						<div class="col-3">
+							
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		<!--<script type="text/javascript" src="./assets/js/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="./assets/js/bootstrap.min.js"></script>-->
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	</body>
+</html>
