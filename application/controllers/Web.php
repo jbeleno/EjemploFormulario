24713fef08cef075b96863bf121181f9f79711ba
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2017-09-27 21:12:24 -0300

    base de datos

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Web extends CI_Controller {
 
+	public function __construct()
+    {
+        parent::__construct();
+	}
+
 	public function index()
 	{
 		$this->load->view('web/index');
